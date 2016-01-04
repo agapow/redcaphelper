f5fd1018c9edaa14af9fd9637d6774360d91e99d
Paul Agapow <paul@agapow.net>
2016-01-04 13:54:18 +0000

    created package skeleton

--- redcaphelper/__init__.py
+++ redcaphelper/__init__.py
@@ -1,1 +1,16 @@
-#
+"""
+Utilities for working with the REDCap database (and the PyCap package)
+"""
+
+__version__ = '0.1'
+
+
+### IMPORTS
+
+from consts import *
+from download import *
+from upload import *
+from utils import *
+
+
+### END ###
--- setup.py
+++ setup.py
@@ -1,26 +1,29 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+from redcaphelper import __version__
 
-setup(name='redcaphelper',
-      version=version,
-      description="Utilities for working with the REDCap database",
-      long_description="""\
-It's useful to programatically work with the REDCap database, e.g. for uploading or downloading data via scripts. This package builds upon the functionality of the redcap package, to expand upon""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='database REDCap pycap',
-      author='Paul Agapow & Kester Jarvis',
-      author_email='paul@agapow.net',
-      url='http://www.agapow.net/software/redcaphelper',
-      license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+setup(
+	name='redcaphelper',
+	version=version,
+	description="Utilities for working with the REDCap database",
+	long_description="""\
+	It's useful to programatically work with the REDCap database. This package
+	builds upon the redcap package, to provide facilities for easy uploading or
+	downloading data via scripts. """,
+	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	keywords='database REDCap pycap',
+	author='Paul Agapow & Kester Jarvis',
+	author_email='paul@agapow.net',
+	url='http://www.agapow.net/software/redcaphelper',
+	license='MIT',
+	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		'redcap',
+	],
+	entry_points="""
+		# -*- Entry points: -*-
+	""",
+)
